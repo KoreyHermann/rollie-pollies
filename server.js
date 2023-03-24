75dc9577fd87920798d15744095233751ceba2e9
Korey Hermann <khermann135@gmail.com>
2023-03-23 20:51:11 -0400

    changed server.js code and added .env file

--- server.js
+++ server.js
@@ -6,20 +6,20 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const routes = require('./routes');
 const sequelize = require('./config/connection');
-// const helpers = require('./utils/helpers');
+const helpers = require('./utils/auth.js');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// const sess = {
-//   secret: 'Super secret secret',
-//   cookie: {},
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize,
-//   }),
-// };
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
 app.use(session(sess));
 
