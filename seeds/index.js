4281d7e19d6a67c35d4d4ed2cb7e072f93d2f583
Ervin543 <ervindasratt@gmail.com>
2023-03-28 21:18:03 -0500

    modified various files for the login page

--- seeds/index.js
+++ seeds/index.js
@@ -1,13 +1,13 @@
 const sequelize = require('../config/connection');
 const seedList = require('./listData');
-const seedPoll = require('./pollData');
+const seedPolls = require('./pollData');
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
 
   await seedList();
 
-  await seedPoll();
+  await seedPolls();
 
   process.exit(0);
 };
