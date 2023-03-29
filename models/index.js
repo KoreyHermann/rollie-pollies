4df93ac343d11fe87cd76694107e66c0a777ce17
Korey Hermann <khermann135@gmail.com>
2023-03-28 20:12:19 -0400

    added code to index.js but for real

--- models/index.js
+++ models/index.js
@@ -1,3 +1,13 @@
 const User = require('./User');
+const poll = require('./poll');
+const list = require('./list');
 
-module.exports = { User }
+list.hasMany(poll, {
+    foreignKey: 'gallery_id',
+  });
+  
+  poll.belongsTo(list, {
+    foreignKey: 'gallery_id',
+  });
+
+module.exports = { User, list, poll };
