780f964209a76a264a4b64cbca9fe6674dfdc14b
Martin Cuellar <martinhc129@gmail.com>
2023-03-30 18:07:01 -0400

    added vote system

--- public/js/polls.js
+++ public/js/polls.js
@@ -22,10 +22,36 @@ createpoll.addEventListener('click', function(event){
 
   const answer1Paragraph = document.createElement('p');
   answer1Paragraph.innerText = inputanswer1.value;
+  
+  const answer1Button = document.createElement('button');
+  answer1Button.innerText = 'Vote';
+  answer1Button.dataset.votes = '0';
+  
+  answer1Button.addEventListener('click', function() {
+    const currentVotes = parseInt(answer1Button.dataset.votes);
+    answer1Button.dataset.votes = currentVotes + 1;
+    answer1Button.innerText = 'Vote (' + (currentVotes + 1) + ')';
+  });
+  
+  answer1Paragraph.appendChild(answer1Button);
+  
   pollanswers.appendChild(answer1Paragraph);
 
   const answer2Paragraph = document.createElement('p');
   answer2Paragraph.innerText = inputanswer2.value;
+  
+  const answer2Button = document.createElement('button');
+  answer2Button.innerText = 'Vote';
+  answer2Button.dataset.votes = '0';
+  
+  answer2Button.addEventListener('click', function() {
+    const currentVotes = parseInt(answer2Button.dataset.votes);
+    answer2Button.dataset.votes = currentVotes + 1;
+    answer2Button.innerText = 'Vote (' + (currentVotes + 1) + ')';
+  });
+  
+  answer2Paragraph.appendChild(answer2Button);
+  
   pollanswers.appendChild(answer2Paragraph);
 
   newPollCard.appendChild(pollanswers);
@@ -41,3 +67,4 @@ createpoll.addEventListener('click', function(event){
 
   pollList.insertBefore(pollListItem, pollList.firstChild);
 });
+
