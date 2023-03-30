f783e60087d56ddf8a7e92c6e127b9175672cd41
Martin Cuellar <martinhc129@gmail.com>
2023-03-30 16:37:26 -0400

    polls are now listed

--- public/js/polls.js
+++ public/js/polls.js
@@ -1,21 +1,43 @@
-const form = document.querySelector('.poll-form');
-const questionInput = document.querySelector('#question');
-const answer1Input = document.querySelector('#answer1');
-const answer2Input = document.querySelector('#answer2');
-const card = document.querySelector('#poll-card');
-const cardQuestion = document.querySelector('#card-question');
-const cardAnswer1 = document.querySelector('#card-answer1');
-const cardAnswer2 = document.querySelector('#card-answer2');
+const createpoll = document.getElementById('addPollbutton');
+const pollList = document.getElementById('poll-list');
 
-form.addEventListener('submit', (event) => {
+createpoll.addEventListener('click', function(event){
+
   event.preventDefault();
 
-  const question = questionInput.value;
-  const answer1 = answer1Input.value;
-  const answer2 = answer2Input.value;
+  const inputquestion = document.getElementById('question');
+  const inputanswer1 = document.getElementById('answer1');
+  const inputanswer2 = document.getElementById('answer2');
 
-  cardQuestion.textContent = question;
-  cardAnswer1.textContent = answer1;
-  cardAnswer2.textContent = answer2;
-  card.style.display = 'block';
+  const newPollCard = document.createElement('div');
+  newPollCard.classList.add('poll-card');
+
+  const pollquestion = document.createElement('h2');
+  pollquestion.id = 'poll-question';
+  pollquestion.innerText = inputquestion.value;
+  newPollCard.appendChild(pollquestion);
+
+  const pollanswers = document.createElement('div');
+  pollanswers.id = 'poll-answers';
+
+  const answer1Paragraph = document.createElement('p');
+  answer1Paragraph.innerText = inputanswer1.value;
+  pollanswers.appendChild(answer1Paragraph);
+
+  const answer2Paragraph = document.createElement('p');
+  answer2Paragraph.innerText = inputanswer2.value;
+  pollanswers.appendChild(answer2Paragraph);
+
+  newPollCard.appendChild(pollanswers);
+
+  newPollCard.style.display = 'block';
+
+  inputquestion.value = '';
+  inputanswer1.value = '';
+  inputanswer2.value = '';
+
+  const pollListItem = document.createElement('li');
+  pollListItem.appendChild(newPollCard);
+
+  pollList.insertBefore(pollListItem, pollList.firstChild);
 });
